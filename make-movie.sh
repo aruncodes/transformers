26bb91dca44ced17219f5722683bd8254c2a3ae5
ARUN BABU <arunb@cse.iitb.ac.in>
2014-11-06 21:53:01 +0530

    Added sound effects to music

--- make-movie.sh
+++ make-movie.sh
@@ -9,6 +9,6 @@ rm -rf *.ppm
 cd ..
 
 # ffmpeg -f image2 -i frames/frame_%4d.jpg output.mp4
-ffmpeg -f image2 -i frames/frame_%4d.jpg -i audio.mp4 -strict -2 -shortest output.mp4
+ffmpeg -f image2 -i frames/frame_%4d.jpg -i audio.mp3 -strict -2 -shortest output.mp4
 
 rm -rf frames/*.jpg
