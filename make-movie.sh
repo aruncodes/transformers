26cc31d54cc81870663d8c97711addbc95eaa2e5
ARUN BABU <arunb@cse.iitb.ac.in>
2014-11-06 14:18:05 +0530

    Added audio to video. Changes to mp4 for better quality.

--- make-movie.sh
+++ make-movie.sh
@@ -4,6 +4,11 @@ cd frames
 
 mogrify -flip -format jpg frame_*.ppm
 
+rm -rf *.ppm
+
 cd ..
 
-ffmpeg -f image2 -i frames/frame_%4d.jpg output.mpg
+# ffmpeg -f image2 -i frames/frame_%4d.jpg output.mp4
+ffmpeg -f image2 -i frames/frame_%4d.jpg -i audio.mp4 -strict -2 -shortest output.mp4
+
+rm -rf frames/*.jpg
